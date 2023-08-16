09fd7ec30ffe1ec2847d12420a776e821ee5e095
prince kumar <theprincesingh07@gmail.com>
2023-08-16 09:05:26 +0530

    done

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ function Time() {
   let minutes = date.getMinutes();
   let seconds = date.getSeconds();
 
+  hours = hours<10?`0${hours}`:`${hours}`
+  minutes = minutes<10?`0${minutes}`:`${minutes}`
+  seconds = seconds<10?`0${seconds}`:`${seconds}`
+
   document.getElementById("hours").innerText = hours;
   document.getElementById("minutes").innerText = minutes;
   document.getElementById("seconds").innerText = seconds;
